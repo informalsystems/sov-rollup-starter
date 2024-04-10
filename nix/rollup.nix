333a1f1af10924c881dea8d9e27ca7ba14b258c8
Soares Chen <soares.chen@maybevoid.com>
2024-04-10 13:40:42 +0200

    Fix mock DA Nix build

--- nix/rollup.nix
+++ nix/rollup.nix
@@ -51,8 +51,8 @@ let
                 "curve25519-dalek-4.1.0" = "sha256-H8YMea3AIcUn9NGRfataNjCTzCK4NAjo4ZhWuPfT6ts=";
                 "sha2-0.10.8" = "sha256-vuFQFlbDXEW+n9+Nx2VeWanggCSd6NZ+GVEDFS9qZ2M=";
                 "risc0-cycle-utils-0.3.0" = "sha256-5dA62v1eqfyZBny4s3YlC2Tty7Yfd/OAVGfTlLBgypk=";
-                "sov-celestia-client-0.1.0" = "sha256-h+J0l1O5V1hO5z9INESs+vwvslbznnhBs9tWWjzm4hU=";
-                "ibc-app-transfer-0.51.0" = "sha256-RBdAlE7rz3g9+0U8Xc5pGiF/vMUOa6CKsp5mUCr70M8=";
+                "sov-celestia-client-0.1.0" = "sha256-51taknkxJLh682KvqFWLwr6Ted1t0LaVL/RcuJhbkWo=";
+                "ibc-app-transfer-0.51.0" = "sha256-6HcMSTcKx4y1hfKcXnzgiNM9FWdqUuUNnemAgS36Z1A=";
             };
         };
 

c5fc573541e6f13fece7eb97a83f6998c36e0397
Ranadeep Biswas <mail@rnbguy.at>
2024-06-07 11:15:14 +0200

    update nix flake lockfiles

--- nix/rollup.nix
+++ nix/rollup.nix
@@ -6,8 +6,8 @@
 ,   risc0-circuit
 }:
 let
-    sov-ibc-hash = "sha256-Mi1fZe109dug9uip1fpK1TqvWBHI8hij8Alkrw4UtUo=";
-    ibc-rs-hash = "sha256-AnRsdl3njOCh0o44GU5uDvX4caxcEitH2Ic3sRu/uIs=";
+    sov-ibc-hash = "sha256-9YsAV1v2/vNgHYnKnSG0XSW7VdvhqgKmUpj8el72F7k=";
+    ibc-rs-hash = "sha256-sKgQB+tKKnIBDqBCf+OdRv72JCdFBB3r7LKNLLQVQS0=";
 
     rollup-guest-src = nixpkgs.stdenv.mkDerivation {
         name = "rollup-guest-src";
